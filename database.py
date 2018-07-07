c4898c23702814ce9daac5d0afb9632a9de3e282
wookienz <y2r42ehg>
2018-07-07 21:18:06 +0800

    alot

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import logging
 
 class database:
     """
@@ -8,12 +9,9 @@ class database:
         """
 
         """
-        self.conn = sqlite3.connect(":memory:")
+        self.conn = sqlite3.connect("database.db")
         self.c = self.conn.cursor()
-        self.c.execute('''CREATE TABLE  kill_tally(name text, kills int)''')
-        self.c.execute('''CREATE TABLE match_id(id text, checked bool )''')
-        self.c.execute('''CREATE TABLE player(name text, account text, matchid text )''')
-        self.conn.commit()
+
 
     def insert_row(self, name):
         """
@@ -60,20 +58,33 @@ class database:
         self.c.execute('SELECT * FROM  match_id')
         return self.c.fetchall()
 
-    def store_name_match(self, match_id, name):
+    def store_name_match(self, match_id, name, account):
         """
         take a name and match id and store in table
-        :param name:
-        :param match_id:
+        :param name: name of player
+        :param match_id: match id as a string
+        :param account: account id of player
         :return:
         """
-        self.c.execute('SELECT name FROM player WHERE (name = ?) AND (matchid = ?)', (name, match_id))
+        self.c.execute('SELECT * FROM player WHERE (name = ?) AND (matchid = ?)', (name, match_id))
         results = self.c.fetchone()
         if results is None:
-            self.c.execute('INSERT INTO player (name, matchid) VALUES (?,?)', (name, match_id))
-            self.c.execute('UPDATE match_id SET id = ?, checked =?', (match_id, True))
+            self.c.execute('INSERT INTO player (name, matchid, account) VALUES (?,?,?)', (name, match_id, account))
             self.conn.commit()
-            return True
+            logging.log(logging.INFO, 'Sotring a name and macth id in player table: %s and %s', name, match_id)
+
+    def mark_match_checked(self, matchid):
+        """
+        IOnce all the playes have been entered from a match id, then mark match checked.
+        :param matchid:
+        :return:
+        """
+        self.c.execute('SELECT * FROM match_id WHERE (id = ?) AND (checked = ?)', (matchid, True))
+        r = self.c.fetchone()
+        if not r:
+            self.c.execute('UPDATE match_id SET checked =? WHERE id = ?', (True, matchid))
+            self.conn.commit()
+            logging.log(logging.INFO, 'Updating macth checked field for match id: %s', matchid)
 
     def return_player_names(self):
         """
@@ -85,10 +96,46 @@ class database:
 
     def return_unchecked_matches(self):
         """
-        Find any matches that have been in stered into the database but have yet to be searched for all the players
+        Find any matches that have been in stored into the database but have yet to be searched for all the players
         in that match
         :return:
         """
         self.c.execute('SELECT id FROM match_id WHERE checked=False')
         l = self.c.fetchall()
+        logging.log(logging.INFO, 'THe following matched have yet to be checked for players: %s', l)
         return list(l)
+
+    def store_name(self, name):
+        """
+        Store a name of a player
+        :param name:
+        :return:
+        """
+        self.c.execute('INSERT INTO player (name) VALUES (?)', (name))
+        self.conn.commit()
+        logging.info(logging.INFO, 'inserting name: %s', name)
+
+    def store_tel_data(self, result, id):
+        """
+
+        :param data:
+        :return:
+        """
+        if not (result.damage_type_category == 'Damage_BlueZone' or result.damage_type_category == 'Damage_Groggy'):
+            self.c.execute('SELECT * FROM kill_data WHERE (match_id = ?) AND (killer = ?) AND (victim = ?)', (id, result.killer.name, result.victim.name))
+            r = self.c.fetchone()
+            if r is None:
+                self.c.execute('INSERT INTO kill_data (match_id, killer, victim) VALUES (?,?,?)', (id, result.killer.name, result.victim.name))
+                self.conn.commit()
+                logging.info(logging.INFO, 'inserting kill data data: %s, %s, %s', id, result.killer.name, result.victim.name)
+
+    def db_init(self):
+        """
+        Initial build of the data base.
+        :return:
+        """
+        self.c.execute('''CREATE TABLE  kill_tally(name text, kills int)''')
+        self.c.execute('''CREATE TABLE match_id(id text, checked bool )''')
+        self.c.execute('''CREATE TABLE player(name text, account text, matchid text )''')
+        self.c.execute('''CREATE TABLE kill_data (killer text victim text match_id text)''')
+        self.conn.commit()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from pubg_python import PUBG, Shard
 import config
 import database
+import json
+import logging
 
+
+logging.basicConfig(filename='nemesis.log', level=logging.DEBUG)
 api = PUBG(config.api_key, Shard.PC_AS)
 d = database.database()
 
@@ -36,6 +40,7 @@ def get_games_from_player_name(name):
     :return:
     """
     players = api.players().filter(player_names=['NZWookie'])
+    logging.log(logging.INFO, 'Looking up player %s, for games they have played: %s', name, players)
     return players
 
 
@@ -55,8 +60,11 @@ def lookup_match_id(id):
     :param id:
     :return:
     """
-    match = api.matches().get(id)
-    return match
+    try:
+        match = api.matches().get(id)
+        return match
+    except Exception as e:
+        return False
 
 
 def store_names_from_match(matchobj):
@@ -66,18 +74,73 @@ def store_names_from_match(matchobj):
     :return:
     """
     for id in match.rosters:
-        d.store_name_match(id.id, id.participants[0].name)
+        d.store_match_id(id.id)
+        d.store_name_match(id.id, id.participants[0].name, id.participants[0].player_id)
+        d.mark_match_checked(id.id)
+
+
+def load_json_from_file(path):
+    """
+    load match ids from a json file
+    :param path:
+    :return:
+    """
+    with open('samples.txt', 'r') as fh:
+        j = json.load(fh)
+        return j
+
+def get_telemetry_data(matchid):
+    """
+
+    :param matchid:
+    :return:
+    """
+    get_match = api.matches().get(matchid)
+    logging.log(logging.INFO, 'getting tel data for match %s', matchid)
+    match_asset = get_match.assets[0]
+    match_tel = api.telemetry(match_asset.url)
+    return match_tel
+
+def store_tel_data(data, id):
+    """
+    Given a telemetry object store relevant data
+    :param data:
+    :return:
+    """
+    l = data.events_from_type('LogPlayerKill')
+    for events in l:
+        logging.log(logging.INFO, 'storing player kill data')
+        d.store_tel_data(events, id)
+
 
 
 test = api.samples().get()
+
+for id in test.matches:
+        tel = get_telemetry_data(id.id)
+        store_tel_data(tel, id.id)
+
+
+"""
+m = load_json_from_file('')
+for a in m['data']['relationships']['matches']['data']:
+    d.store_match_id(a['id'])
+
+
 games_played = get_games_from_player_name("NZWookie")
 store_match_id(games_played)
+
+
 ids = d.return_match_ids()
 for a in ids:
     match = lookup_match_id(a[0])
-    store_names_from_match(match)
+    if match:
+        store_names_from_match(match)
+
+
 names = d.return_player_names()
 for a in names:
     games_played = get_games_from_player_name(a[0])
     store_match_id(games_played)
 print(d.return_player_names())
+"""
